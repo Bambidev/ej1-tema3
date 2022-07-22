97abca20bdaff07f2a8eabb0db930ff143852b78
Nicolas Orieta <orietasebastian@hotmail.com>
2022-07-22 20:24:39 -0300

    Archivos

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        suma(50,50,40);
+
+        coche miChoche = new coche();
+        miChoche.agregarPuerta();
+        System.out.println("Cantidad de puertas: "+ miChoche.puertas);
+    }
+
+    public static void suma(int a, int b, int c){
+        System.out.println(a + b + c);
+    }
+
+}
+
+class coche {
+    public int puertas = 4;
+    public void agregarPuerta() {
+        this.puertas++;
+    }
+}
